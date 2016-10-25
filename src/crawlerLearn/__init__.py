73bad518a58e7324b2a2ff31388df79f2f0085e1
ChaosIllusion <331258298@qq.com>
2016-10-25 23:51:03 +0800

    learning concepts...

--- src/crawlerLearn/__init__.py
+++ src/crawlerLearn/__init__.py
@@ -1,4 +1,17 @@
 import urllib.request as urllib2
 
-response = urllib2.urlopen("http://www.baidu.com")
-print (response.read())
+
+request = urllib2.Request('http://blog.csdn.net/cqcre')
+try:
+    response = urllib2.urlopen(request)
+except urllib2.HTTPError as e:
+    if hasattr(e, "code"):
+        print (e.code)
+except urllib2.URLError as e:
+    if hasattr(e, "reason"):
+        print (e.reason)
+else:
+    print("OK")
+    print (response.read())    
+
+
